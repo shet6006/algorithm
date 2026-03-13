b0c84d238eecb8f8dc6b90dcc9bc4f211624ee48
Ddong <shet6006@gmail.com>
2026-03-13 17:42:36 +0900

    문제 해결

--- 백준/5430.py
+++ 백준/5430.py
@@ -1,1 +1,31 @@
+from collections import deque
 # reverse flag와 deque이용해서 풀기
+tc = int(input())
+for i in range(tc):
+    op = input()
+    n = int(input())
+    arr = input()[1:-1].split(',')
+    queue = deque(arr)
+    flag = True #정방향
+
+    if n == 0:
+        queue = []
+
+    for operand in op:
+        if operand == 'R':
+            flag = not flag
+        elif operand == 'D':
+            if len(queue) == 0:
+                print("error")
+                break
+            else:
+                if flag:
+                    queue.popleft()
+                else:
+                    queue.pop()
+    else:
+        if flag:
+            print("[" + ",".join(queue) + "]")
+        else:
+            queue.reverse()
+            print("[" + ",".join(queue) + "]")
