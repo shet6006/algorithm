a6461086251ffa00fa8579d8d2e378713fafebc9
shet6006 <shet6006@gmail.com>
2026-04-01 20:08:15 +0900

    문제 해결

--- 백준/18110.py
+++ 백준/18110.py
@@ -1,13 +1,18 @@
 import math
 
 n = int(input())
-exclude_n = round(n * 0.15)
+
+if n == 0:
+    print(0)
+    exit()
+
+exclude_n = int(n * 0.15 + 0.5)
 
 level = []
 for i in range(n):
     level.append(int(input()))
 
-level.sort() 
+level.sort()
 
 result = 0
 up = n - exclude_n
@@ -17,7 +22,4 @@ for i in range(exclude_n, up):
 
 count = n - (2 * exclude_n)
 
-if count == 0:
-    print(0)
-else:
-    print(round(result / count))
+print(int(result / count + 0.5))
